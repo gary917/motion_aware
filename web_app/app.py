c105fefcb317f0e4ea2d815902d10fbe03b016af
gary917 <gary991177@gmail.com>
2018-08-09 21:41:33 +1000

    api to send data to server on alibaba cloud now working

--- web_app/app.py
+++ web_app/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, request
 import sys
 import sqlite3
+import mysql.connector
 
 app = Flask(__name__)
-DB_LOCATION = app.root_path + '/db/motion_aware.db'
+
+local_DB_LOCATION = app.root_path + '/db/motion_aware.db'
+
+
+db_config = {
+    'user': 'admin_1',
+    'password' : 'Motionaware1',
+    'host': 'rm-j0bz21fg6h05n786lro.mysql.australia.rds.aliyuncs.com',
+    'database' : 'motion_aware_apsara'
+}
 
 
 
 @app.route("/")
 def main():
-    #print(app.root_path, file = sys.stderr)
     return render_template('sign_in.html')
-
 
 
 @app.route('/api', methods=['PUT']) # function to accept POST
@@ -22,7 +30,7 @@ def post_function():
     name = post_data['name']
     password = post_data['password']
 
-    conn = sqlite3.connect(DB_LOCATION, timeout=10)
+    conn = sqlite3.connect(local_DB_LOCATION, timeout=10)
     c = conn.cursor()
     entry = (user_id, username, password, name)
     try:
@@ -34,27 +42,35 @@ def post_function():
     conn.close()
     return "USER ADDED"
 
-@app.route('/api/data', methods=['PUT']) # function to accept POST
-def put_data():
-    post_data = request.get_json()
 
+@app.route('/api/mysql', methods=['PUT'])  # function to accept POST
+def my_sql():
+
+    cnx = mysql.connector.connect(**db_config)
+    cursor = cnx.cursor()
+
+    post_data = request.get_json()
     date = post_data['date']
     time_stamp = post_data['time_stamp']
     device_id = post_data['device_id']
     acceleration = post_data['acceleration']
     status = post_data['status']
 
-    conn = sqlite3.connect(DB_LOCATION, timeout=10)
-    c = conn.cursor()
-    entry = (date, time_stamp, device_id, acceleration, status)
+    insert_reading = ("INSERT INTO readings"
+                      "(date, time_stamp, device_id, acceleration, status)"  # name of column fields
+                      "VALUES (%s,%s,%s,%s,%s)")
+    data = (date, time_stamp, device_id, acceleration, status)
+
     try:
-        c.execute("INSERT INTO readings VALUES (?,?,?,?,?)", entry)
-    except sqlite3.IntegrityError:       # duplicating unique fields
+        cursor.execute(insert_reading, data)
+    except sqlite3.IntegrityError:
         return "DATA NOT INSERTED"
 
-    conn.commit() # save changes to database
-    conn.close()
-    return "DATA INSERTED"
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    return "DATA ADDED"
+
 
 if __name__ == "__main__":
 	app.run(host = '0.0.0.0')
